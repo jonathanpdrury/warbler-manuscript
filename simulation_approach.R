ebaa0fc831912c39a6e8107c999b96033ddabacb
jonathanpdrury <jonathan.p.drury@gmail.com>
2015-11-10 14:45:39 +0100

    Update simulation_approach.R

--- simulation_approach.R
+++ simulation_approach.R
@@ -17,7 +17,7 @@ N<-5000 #number of simulations
 ##next, generate data, first using fitContinuous to estimate sigma2, then adding a as mean and bounds set at the max and min values +-10%
 
 #mass, centering data with z transformation (xi-xbar/sdev), bounds set as 10% of the upper and lower bounds of empirical z scores
-simdata.mass<-fastBM(tree,a=0,sig2= 0.016434, bounds=c(-1.567149, 3.491771),nsim=N)
+simdata.mass<-fastBM(tree,a=0,sig2=0.015852 , bounds=c(-1.325469,3.318858 ),nsim=N)
 variable2.matrix<-var.matrix(simdata.mass)
 variable2.matrix<-sqrt(variable2.matrix)
 for (i in 1:N){
@@ -25,7 +25,7 @@ for (i in 1:N){
 	} #z transform
 
 #now using Z score transfomration to simulate bill, bounds set as 10% of the upper and lower bounds of empirical z scores
-simdata.bill<-fastBM(tree,sig2= 0.022336,a= 0,bounds=c(-1.734458, 2.938706), nsim=N)
+simdata.bill<-fastBM(tree,sig2=0.019364 ,a= 0,bounds=c(-1.352945,3.518544), nsim=N)
 variable3.matrix<-var.matrix(simdata.bill)
 variable3.matrix<-sqrt(variable3.matrix)
 for (i in 1:N){
@@ -36,11 +36,11 @@ for (i in 1:N){
 #as two continuous variables, and then the euclidian distance is calculated between species for two values, rather than one
 
 
-R<-0.044672 #rate parameter to use min=0.016434 max=0.044672
+R<-0.015852 #rate parameter to use min=0.015852 max=0.038728
 #plumage similarity 
 simdata.1<-fastBM(tree,sig2= R,a= 0,nsim=N)
 simdata.2<-fastBM(tree,sig2= R, a= 0,nsim=N)
-simdata.plumarray<-array(dim=c(42,N,2)) #42 is number of tips, N is number of simulations, 2 is number of variables simulated
+simdata.plumarray<-array(dim=c(44,N,2)) #44 is number of tips, N is number of simulations, 2 is number of variables simulated
 simdata.plumarray[,,1]<-simdata.1
 simdata.plumarray[,,2]<-simdata.2
 variable4.matrix<-var.mv.matrix(simdata.plumarray)
@@ -53,7 +53,7 @@ for (i in 1:N){
 #song similarity
 simdata.1<-fastBM(tree,sig2= R,a= 0,nsim=N)
 simdata.2<-fastBM(tree,sig2= R, a= 0,nsim=N)
-simdata.humsongarray<-array(dim=c(42,N,2)) #42 is number of tips, N is number of simulations, 2 is number of variables simulated
+simdata.humsongarray<-array(dim=c(44,N,2)) #44 is number of tips, N is number of simulations, 2 is number of variables simulated
 simdata.humsongarray[,,1]<-simdata.1
 simdata.humsongarray[,,2]<-simdata.2
 variable5.matrix<-var.mv.matrix(simdata.humsongarray)
@@ -66,7 +66,7 @@ for (i in 1:N){
 #spcc data 
 simdata.1<-fastBM(tree,sig2= R,a= 0,nsim=N)
 simdata.2<-fastBM(tree,sig2= R,a= 0,nsim=N)
-simdata.songarray<-array(dim=c(42,N,2)) #42 is number of tips, N is number of simulations, 2 is number of variables simulated
+simdata.songarray<-array(dim=c(44,N,2)) #44 is number of tips, N is number of simulations, 2 is number of variables simulated
 simdata.songarray[,,1]<-simdata.1
 simdata.songarray[,,2]<-simdata.2
 
@@ -80,30 +80,30 @@ for (i in 1:N){
 	} #z transform
 
 
-#guild overlap (0.2931034 [85] are 1's)
+#guild overlap (0.3442623 [105] are 1's)
 simdata.guild<-fastBM(tree,sig2=R,a=0,nsim=N)
 variable6.matrix<-var.matrix(simdata.guild)
 
-#this loop pulls out 85 differences for each run of the code for identifying as 1's (note: this loop is computationally expensive, and takes several minutes to run)
-int.matrix<-matrix(nrow=290,ncol=N)
+#this loop pulls out 105 differences for each run of the code for identifying as 1's (note: this loop is computationally expensive, and takes several minutes to run)
+int.matrix<-matrix(nrow=305,ncol=N)
 for(j in 1:N){
 	i=1
-	while(i<291){
-		int.matrix[i,j]<-ifelse(variable6.matrix[i,j]<quantile(variable6.matrix[,j],c(0.2931034)),1,0) 
+	while(i<306){
+		int.matrix[i,j]<-ifelse(variable6.matrix[i,j]<quantile(variable6.matrix[,j],c(0.3442623)),1,0) 
 		i=i+1
 		}
 	}
 variable6.matrix<-int.matrix
 	
-#hybridization, 24 pairs, 0.08275862
+#hybridization, 24 pairs, 0.07868852
 simdata.hybrid<-fastBM(tree,sig2=R,a=0,nsim=N)
 variable8.matrix<-var.matrix(simdata.hybrid)
 #this loop pulls out 24 differences for each run of the code for identifying as 1's (note: this loop is computationally expensive, and takes several minutes to run)
-int.matrix<-matrix(nrow=290,ncol=N)
+int.matrix<-matrix(nrow=305,ncol=N)
 for(j in 1:N){
 	i=1
-	while(i<291){
-		int.matrix[i,j]<-ifelse(variable7.matrix[i,j]<quantile(variable7.matrix[,j],c(.08275862)),1,0)
+	while(i<306){
+		int.matrix[i,j]<-ifelse(variable7.matrix[i,j]<quantile(variable7.matrix[,j],c(0.07868852)),1,0)
 		i=i+1
 		}
 	}
@@ -147,16 +147,35 @@ var7.CI
 var8.CI
 var9.CI
 
+var2.95CI<-quantile(results.matrix[2,],c(0.025,0.975))
+var3.95CI<-quantile(results.matrix[3,],c(0.025,0.975))
+var4.95CI<-quantile(results.matrix[4,],c(0.025,0.975))
+var5.95CI<-quantile(results.matrix[5,],c(0.025,0.975))
+var6.95CI<-quantile(results.matrix[6,],c(0.025,0.975))
+var7.95CI<-quantile(results.matrix[7,],c(0.025,0.975))
+var8.95CI<-quantile(results.matrix[8,],c(0.025,0.975))
+var9.95CI<-quantile(results.matrix[9,],c(0.025,0.975))
+
+var2.95CI
+var3.95CI
+var4.95CI
+var5.95CI
+var6.95CI
+var7.95CI
+var8.95CI
+var9.95CI
+
 
 ##just running the best model now:
 
 results.matrix<-matrix(ncol=N,nrow=5)
 for (i in 1:N){
-	m1<-glm(data$all.IT~variable4.matrix[,i]+data$Zsyn+variable7.matrix[,i],family="binomial")
+	m1<-glm(data$all.IT~variable2.matrix[,i]+variable4.matrix[,i]+data$Zsyn+variable7.matrix[,i],family="binomial")
 	results.matrix[1,i]<-as.numeric(summary(m1)[5]) #this pulls out the aic score
 	results.matrix[2,i]<-as.numeric(coef(summary(m1))[2,3]) ##this grabs the z value for the first predictor variable in a logistic regression
 	results.matrix[3,i]<-as.numeric(coef(summary(m1))[3,3])
 	results.matrix[4,i]<-as.numeric(coef(summary(m1))[4,3])
+	results.matrix[5,i]<-as.numeric(coef(summary(m1))[5,3])
 
 	}
 	
@@ -166,8 +185,18 @@ aic.CI<-quantile(results.matrix[1,],c(0.05,0.95))
 var2.CI90<-quantile(results.matrix[2,],c(0.05,0.95))
 var3.CI90<-quantile(results.matrix[3,],c(0.05,0.95))
 var4.CI90<-quantile(results.matrix[4,],c(0.05,0.95))
+var5.CI90<-quantile(results.matrix[5,],c(0.05,0.95))
+var2.CI95<-quantile(results.matrix[2,],c(0.025,0.975))
+var3.CI95<-quantile(results.matrix[3,],c(0.025,0.975))
+var4.CI95<-quantile(results.matrix[4,],c(0.025,0.975))
+var5.CI95<-quantile(results.matrix[5,],c(0.025,0.975))
 
 var2.CI90
 var3.CI90
 var4.CI90
+var5.CI90
+var2.CI95
+var3.CI95
+var4.CI95
+var5.CI95
 
